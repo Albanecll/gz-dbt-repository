08b91be54963a4c1ec6c9ca229513705e7e4f2ea
albanecallec@gmail.com <albanecallec@gmail.com>
2026-02-05 15:51:46 +0000

    Creation of finance_campaigns_month.sql Mart Model

---
+++ models/mart/finance/finance_campaigns_month.sql
@@ -0,0 +1,22 @@
+{{ config(materialized='table') }}
+
+select
+extract(month from date) as datemonth,
+
+  sum(ads_margin) as ads_margin,
+  safe_divide(sum(revenue), nullif(sum(quantity), 0)) as average_basket,
+  sum(operational_margin) as operational_margin,
+  sum(ads_cost) as ads_cost,
+  sum(ads_impression) as ads_impression,
+  sum(ads_clicks) as ads_clicks,
+  sum(quantity) as quantity,
+  sum(revenue) as revenue,
+  sum(purchase_cost) as purchase_cost,
+  sum(margin) as margin,
+  sum(shipping_fee) as shipping_fee,
+  sum(log_cost) as log_cost,
+  sum(ship_cost) as ship_cost
+
+from {{ ref('finance_campaigns_day') }}
+group by datemonth
+order by datemonth desc
